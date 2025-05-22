28b65334561e7bf901f97bfcd255e0d3925bc305
DeletedWasTaken <81261795+DeletedWasTaken@users.noreply.github.com>
2025-05-22 18:19:36 -0400

    first_nav_update

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -6,12 +6,29 @@ const Navbar = () => {
 
     const [selected, setSelected] = useState("0")
     const [hovering, sethovering] = useState("0")
-    const buttons = [["FOR YOU", "/"], ["CLINICAL STUDIES", "/clinical"], ["ABOUT", "/"]]
+    const buttons = [["Dashboard", "/home"], ["Articles", "/articles"], ["Studies", "/clinical"], ["About", "/about"], ["Settings", "/settings"]]
     const nav = useNavigate();
 
     return (
-        <div className="mainNav1">
-            
+        <div className="navspacer">
+            <div className="mainNav1">
+                <div className='logospace'>
+
+                </div>
+                <div className="sections">
+                {
+                    buttons.map((info) => ( // info = one array in the buttons array. info[0] is text, info[1] is a link
+                        <div onClick={() => {
+                            setSelected(info[0])
+                            nav(info[1])
+                            }} onMouseEnter={() => sethovering(info[0])} onMouseLeave={() => sethovering("")} className={`pageselector ${selected === info[0]  ? "active" : (hovering === info[0] ? "hovering" : "")}`}>
+                            <div className="selectorimg"></div>
+                            <h1 className={`selectortext ${selected === info[0]  ? "active" : (hovering === info[0] ? "hovering" : "")}`}>{info[0]}</h1>
+                        </div>
+                    ))
+                }
+                </div>
+
             {/* <div className="navbox left">
 
             </div>
@@ -30,7 +47,9 @@ const Navbar = () => {
             <div className="navbox right">
 
             </div> */}
+            </div>
         </div>
+        
     )   
 }
 
