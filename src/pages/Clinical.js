bc215898b193b10da1eafd28966bf0c346b8cdc4
JasonPatrickSmith <jasonpatricksmith00@gmail.com>
2025-06-02 21:34:19 -0400

    beginning of constant card

--- src/pages/Clinical.js
+++ src/pages/Clinical.js
@@ -8,9 +8,8 @@ const Clinical = () => {
     const [recommended, setRecommended] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
     const [date, setDate] = useState("1M")
     const [studies, setStudies] = useState([])
-
     const datesortings = [["1M", 30],["1Y", 365],["5Y", 1825],["10Y", 3650],["30Y", 10950], ["ALL", 100000]]
-    
+
     useEffect(() => {
         const fetchStudies = async () => {
           const { data, error } = await supabase
@@ -21,12 +20,11 @@ const Clinical = () => {
             console.error('Error fetching studies:', error)
           } else {
             setStudies(data)
-            console.log(data)
           }
         }
     
         fetchStudies()
-      }, [])
+      }, [date])
 
     return (
         <div className="hidescroll mainclin">
@@ -63,7 +61,47 @@ const Clinical = () => {
                             ))
                         }
                         </div>
-                        
+                    </div>
+                    <div className="directory">
+                        <div className="constants">
+                            <div className="constantcard first"></div>
+                            {
+                                studies.map((info, i) => (
+                                    <div className="constantcard card">
+                                        <div className="constantmargin">
+                                            <div className="approval"></div>
+                                            <div className="headertext">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <div className="infowrapper">
+                            <div className="info">
+                                <div className="studycard first">
+                                    <div className="condition">Condition</div>
+                                    <div className="treatment">Treatment</div>
+                                    <div className="sample">Sample Size</div>
+                                    <div className="duration">Duration</div>
+                                    <div className="date">Date</div>
+                                    <div className="views">Views</div>
+                                </div>
+                                {
+                                    studies.map((info, i) => (
+                                        <div className="studycard card">
+                                            <div className="condition">Herniated Disc</div>
+                                            <div className="treatment">Epidural Steroid...</div>
+                                            <div className="sample">100</div>
+                                            <div className="duration">1 Year</div>
+                                            <div className="date">Mar 15</div>
+                                            <div className="views"> 1324</div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
